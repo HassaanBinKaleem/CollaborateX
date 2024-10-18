e81c88c347be725702dbd92ecb9f2a3aae6c2d23
Hassaan-Bin-Kaleem <hassaanbinkaleem216@gmail.com>
2024-10-18 17:47:13 +0500

    frontend setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function App(): React.JSX.Element {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <React.Fragment>
+      <div>
+        <h1 className="text-center font-bold text-5xl mx-auto mt-[50dvh]">CollaborateX</h1>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default App;

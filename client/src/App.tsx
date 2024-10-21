72602cbd50bc2ca0a75cbdd47d49503ca1309bbd
Hassaan-Bin-Kaleem <105790628+HassaanBinKaleem@users.noreply.github.com>
2024-10-21 23:13:55 +0500

    Deployment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,13 @@ import React from "react";
 function App(): React.JSX.Element {
   return (
     <React.Fragment>
-      <div>
-        <h1 className="text-center font-bold text-5xl mx-auto mt-[50dvh]">CollaborateX</h1>
+      <div className="bg-black">
+        <div className="px-auto py-[42.5dvh]">
+          <h1 className="text-center font-bold text-5xl text-white">
+            CollaborateX
+          </h1>
+          <p className="text-white text-center mt-6">Coming Soon</p>
+        </div>
       </div>
     </React.Fragment>
   );

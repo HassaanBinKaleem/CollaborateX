56d8ea25abcabf59fb84fdf05029950c5364785a
Hassaan-Bin-Kaleem <105790628+HassaanBinKaleem@users.noreply.github.com>
2024-10-24 12:59:54 +0500

    Deployment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React from 'react';
+import Confetti from 'react-confetti';
 
 function App(): React.JSX.Element {
   return (
     <React.Fragment>
-      <div>
-        <div className="px-auto pt-[50dvh]">
-          <h1 className="text-center font-bold text-5xl text-white">
-            CollaborateX
-          </h1>
-          <p className="text-white text-center mt-6">Coming Soon</p>
-        </div>
+      <Confetti
+        width={window.innerWidth}
+        height={window.innerHeight}
+        numberOfPieces={400}
+        recycle={false}  
+        gravity={0.2}   
+      />
+      <div className="px-auto pt-[50vh]">
+        <h1 className="text-center font-bold text-5xl text-white">
+          CollaborateX
+        </h1>
+        <p className="text-white text-center mt-6">Coming Soon</p>
       </div>
     </React.Fragment>
   );
 }
 
 export default App;
+
